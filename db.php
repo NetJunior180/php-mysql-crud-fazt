9622ec6d7200df643ac4758f376ec68cf2e38fad
NetJunior180 <netjuni0r180@gmail.com>
2024-05-23 18:40:48 -0500

    V5

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
-    $dbhost = DB_HOST;
-    $dbname = DB_DATABASE;
-    $dbuname = DB_USERNAME;
-    $dbpass = DB_PASSWORD;
-    $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
 
-    $conn = mysqli_init();
-    mysqli_real_connect($conn, $dbhost, $dbuname, $dbpass, $dbname, 3306, $ssl_cert);
-
-    if (mysqli_connect_errno()) {
-      die("Connection failed: " . mysqli_connect_error());
-    }
-?>
+$db_host = DB_HOST;
+$db_user = DB_USERNAME;
+$db_password = DB_PASSWORD;
+$db_name = DB_DATABASE;
+$ssl_cert = "ssl/DigiCertGlobalRootCA.crt.pem"; // Replace with your SSL certificate path
 
 // Connect to the database with SSL
+$conn = mysqli_init();
+mysqli_real_connect($conn, $db_host, $db_user, $db_password, $db_name, 3306, $ssl_cert);
+
+if (mysqli_connect_errno()) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
