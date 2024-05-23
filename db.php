b22ab35fc5380c5ac241d40088f45342fa32af82
NetJunior180 <netjuni0r180@gmail.com>
2024-05-23 18:37:27 -0500

    V4

--- db.php
+++ db.php
@@ -6,7 +6,7 @@
     $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
 
     $conn = mysqli_init();
-    mysqli_real_connect($conn, $db_host, $db_user, $db_password, $db_name, 3306, $ssl_cert);
+    mysqli_real_connect($conn, $dbhost, $dbuname, $dbpass, $dbname, 3306, $ssl_cert);
 
     if (mysqli_connect_errno()) {
       die("Connection failed: " . mysqli_connect_error());
